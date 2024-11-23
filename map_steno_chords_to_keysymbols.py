597a227ffca075c33b732f7cffae9838a229cb92
StenoHarri <159228685+StenoHarri@users.noreply.github.com>
2024-11-23 22:16:31 +0000

    Update map_steno_chords_to_keysymbols.py

--- map_steno_chords_to_keysymbols.py
+++ map_steno_chords_to_keysymbols.py
@@ -9,6 +9,10 @@ Making the right hand lowercase so I don't have to worry about left P vs right P
 import re
 
 
+y_chord = "KWH" # the 'y' in yet or the 'i' in genius
+silent_linker = "KWR" # the 'linker' in genus
+
+
 keysymbol_shorthands = {
     """
     There will be groups of keysymbols that come up again and again,
@@ -123,7 +127,7 @@ steno_chords_and_their_meanings = {
 
 
     "/K": [
-        {"description": "K for initial k",
+        {"description": "K for initial/linking k",
          "spelling": "k",
          "pronunciation": " (starting_)?((root)|(prefix)|(suffix))  k ",
          "ambiguity": 0,
@@ -131,7 +135,7 @@ steno_chords_and_their_meanings = {
          "what does it affect": ['root', 'suffix', 'prefix'],
          "steno theory": "WSI"},
 
-         {"description": "K for initial hard c",
+         {"description": "K for initial/linking hard c",
          "spelling": "c",
          "pronunciation": " (starting_)?((root)|(prefix)|(suffix))  k ",
          "ambiguity": 1,
@@ -155,6 +159,33 @@ steno_chords_and_their_meanings = {
          "what must come before": ".*[/S]\*?",
          "what does it affect": ['root', 'suffix', 'prefix'],
          "steno theory": "WSI"}],
+
+
+    "/"+silent_linker: [
+        {"description": silent_linker + " for silent linker",
+         "spelling": "",
+         "pronunciation": " ((root)|(prefix)|(suffix)) ",
+         "ambiguity": 0,
+         "what must come before": ".*[frpblgtsdz]\*?",
+         "what does it affect": ['root', 'suffix', 'prefix'],
+         "steno theory": "User preference"},
+
+         {"description": silent_linker + " for silent linker that ignores word boundaries",
+         "spelling": "",
+         "pronunciation": "",
+         "ambiguity": 1,
+         "what must come before": ".*[frpblgtsdz]\*?",
+         "what does it affect": ['root', 'suffix', 'prefix'],
+         "steno theory": "User preference"}],
+
+    "/"+y_chord: [
+        {"description": y_chord + " for initial/linking y",
+         "spelling": "y",
+         "pronunciation": " ((root)|(prefix)|(suffix))?  iy ",
+         "ambiguity": 0,
+         "what must come before": ".*",
+         "what does it affect": ['root', 'suffix', 'prefix'],
+         "steno theory": "User preference"}],
 
     "HR": [
         {"description": "HR for l",
@@ -191,6 +222,15 @@ steno_chords_and_their_meanings = {
          "what does it affect": ['root', 'suffix', 'prefix'],
          "steno theory": "WSI"}],
 
+    "eu": [
+        {"description": "EU for y said like long e",
+         "spelling": "y",
+         "pronunciation": " iy ",
+         "ambiguity": 0,
+         "what must come before": ".*[STKPWHR]+\*?",
+         "what does it affected": ['root', 'suffix', 'prefix'],
+         "steno theory": "I think StenEd?"}],
+
 
     "d": [
         {"description": "-D for d",
@@ -211,12 +251,12 @@ pronunciation_dictionary={
         "incomplete steno":[
             {
                 'built-up':{
-                    "chords":"K", 
+                    "steno outline":"K", 
                     "pronunciation":" k ",
                     "spelling":"c",
                     "ambiguity": 0,
                     "steno theories": ["WSI"]},
-                'breakdown':[
+                'explanation of each chord':[
                     {
                         "chord":"K",
                         "pronunciation":" k ",
@@ -229,12 +269,12 @@ pronunciation_dictionary={
             ,
             {
                 'built-up':{
-                    "chords":"KHR",
+                    "steno outline":"KHR",
                     "pronunciation":" k l ",
                     "spelling":"cl",
                     "ambiguity": 0,
                     "steno theories": ["WSI"]},
-                'breakdown':[
+                'explanation of each chord':[
                     {
                         "chord":"K",
                         "pronunciation":" k ",
@@ -267,56 +307,73 @@ def add_pronunciation_to_pronunciation(old_pronunciation, new_pronunciation, cri
         return  old_pronunciation+new_pronunciation
     return False
 
+"""
 def make_target_pronunciation_into_string(target_list):
-    string=" starting_"
+    string=" starting__"
     for morpheme in target_list:
         #print(morpheme[1][0]) would print stuff like root root root root
-        string+=morpheme[1][0] + "  " + "  ".join(morpheme[0]) + " "
-    return string
-
+        string+=' '+morpheme[1][0] + "  " + "  ".join(morpheme[0]) + " "
+    return string.replace('_ ','')
+"""
 
 def add_pronunciation_to_pronunciation(old_pronunciation, new_pronunciation, target):
     criteria = re.compile(old_pronunciation + new_pronunciation +".*")
 
-    target = make_target_pronunciation_into_string(target)
+    target
 
     if criteria.fullmatch(target):
         return old_pronunciation+new_pronunciation
     return False
 
+"""
 def make_target_spelling_into_string(target_list):
     string=""
     for morpheme in target_list:
         string+="Something went wrong".join(morpheme[0])
     return string
-
+"""
+    
 def add_spelling_to_spelling(old_spelling, new_spelling, target):
     criteria = re.compile(old_spelling + new_spelling + ".*")
 
-    target = make_target_spelling_into_string(target)
+    target
 
     if criteria.fullmatch(target):
         return old_spelling+new_spelling
 
     return False
 
-def add_a_chord_onto_each_incomplete_entry(initial_dictionary, target_pronunciation, target_spelling, never_seen_before_entries=[]):
+def is_entry_complete(entry, pronunciation_target, spelling_target):
+
+    if not re.fullmatch(r'.*[AOeufrpblgtsdz]\*?', entry['built-up']["steno outline"]):
+        return False
+
+    pronunciation_regex_attempt = re.compile(entry['built-up']['pronunciation'])
+    spelling_regex_attempt = re.compile(entry['built-up']['spelling'])
+    if pronunciation_regex_attempt.fullmatch(pronunciation_target) and spelling_regex_attempt.fullmatch(spelling_target):
+
+        
+        return {"steno outline": entry["built-up"]["steno outline"],
+                "ambiguity":entry["built-up"]["ambiguity"]},
+
+    return False
+
+
+def add_a_chord_onto_each_incomplete_entry(initial_dictionary, target_pronunciation, target_spelling, never_seen_before_entries=[], every_complete_entry_generated=[]):
 
     dictionary_with_a_chord_added_to_each_entry =[]
     for entry in initial_dictionary:
         for chord in steno_chords_and_their_meanings:
             
             #Um actually it's fine to break steno order
-            #if not is_steno_order.match(entry["built-up"]["chords"] + chord):
+            #if not is_steno_order.match(entry["built-up"]["steno outline"] + chord):
             #    next
 
             for chord_interpretation in steno_chords_and_their_meanings[chord]:
-                
-                
-                chords = add_chord_to_chords(entry["built-up"]["chords"], chord, chord_interpretation["what must come before"])
+
+                chords = add_chord_to_chords(entry["built-up"]["steno outline"], chord, chord_interpretation["what must come before"])
                 if not chords:
                     continue
-                
 
                 pronunciation = add_pronunciation_to_pronunciation(entry["built-up"]["pronunciation"], chord_interpretation["pronunciation"], target_pronunciation)
                 if not pronunciation:
@@ -328,28 +385,35 @@ def add_a_chord_onto_each_incomplete_entry(initial_dictionary, target_pronunciat
 
                 ambiguity = entry["built-up"]["ambiguity"] + chord_interpretation["ambiguity"]
 
-                steno_theories = entry["built-up"]["steno theories"]#.append(chord_interpretation["steno theory"])
-                steno_theories.append(chord_interpretation["steno theory"])
+
 
                 dictionary_with_a_chord_added_to_each_entry+=[{
                     "built-up":{
-                        "chords":chords,
+                        "steno outline":chords,
                         "pronunciation":pronunciation,
                         "spelling":spelling,
-                        "ambiguity":ambiguity,
-                        "steno theories":steno_theories},
-                    "breakdown":[]
+                        "ambiguity":ambiguity,},
+                    "explanation of each chord":[]
                 }]
 
     new_never_seen_before_entries = []
     for entry in dictionary_with_a_chord_added_to_each_entry:
         if not entry in initial_dictionary or never_seen_before_entries:
-            new_never_seen_before_entries.append(entry)
+            #print("added "+entry['built-up']["steno outline"], entry['built-up']['spelling'])
+
+
+            #remember that a valid entry can still be added to... maybe not?
+            #actually yeah it can't, because /K and /K can be different chords
+            is_entry_complete_answer = is_entry_complete(entry, target_pronunciation, target_spelling)
+            if is_entry_complete_answer:
+                every_complete_entry_generated.append(is_entry_complete_answer)
+            else:
+                new_never_seen_before_entries.append(entry)
 
     if new_never_seen_before_entries:
-        never_seen_before_entries = (add_a_chord_onto_each_incomplete_entry(new_never_seen_before_entries, target_pronunciation, target_spelling, new_never_seen_before_entries))
+        never_seen_before_entries, every_complete_entry_generated = (add_a_chord_onto_each_incomplete_entry(new_never_seen_before_entries, target_pronunciation, target_spelling, new_never_seen_before_entries, every_complete_entry_generated))
     
-    return never_seen_before_entries
+    return never_seen_before_entries, every_complete_entry_generated
 
 
 
@@ -359,31 +423,22 @@ def generate_write_outs(input_word):
     list_of_incomplete_entries = [
         {
         "built-up":{
-            "chords":"",
+            "steno outline":"",
             "pronunciation":"",
             "spelling":"",
-            "ambiguity": 0,
-            "steno theories": []},
-        "breakdown":[]
+            "ambiguity": 0},
+        "explanation of each chord":[]
         }
     ]
 
+    #print(input_word['word'])
+    last_entry_generated ,list_of_incomplete_entries = add_a_chord_onto_each_incomplete_entry(list_of_incomplete_entries, input_word['pronunciation'], input_word['word_boundaries'], every_complete_entry_generated=[])
 
-    list_of_incomplete_entries = add_a_chord_onto_each_incomplete_entry(list_of_incomplete_entries, input_word['pronunciation'], input_word['word_boundaries'])
-
-
-    print(list_of_incomplete_entries)
+    #if not list_of_incomplete_entries==[]:
     
-
-    
-
+    return(list_of_incomplete_entries)
 
 
 
-
-
-
-        
 
 #print(generate_write_outs("test"))
-
