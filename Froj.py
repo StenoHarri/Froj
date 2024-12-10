3e2c5255678d7961f2724d0229acb51e28d485b5
StenoHarri <159228685+StenoHarri@users.noreply.github.com>
2024-12-10 16:52:32 +0000

    Feeding in the dictionary!
    This has taken the program from taking 11 minutes to taking just 2!!!!

--- Froj.py
+++ Froj.py
@@ -8,6 +8,19 @@ starting_root  l  i  .  k  w  I2  =.=  f  a  k  .  sh  ==  n ",
   "word_boundaries": "lique==fact==ion",
 
 
+parallelepiped
+
+  
+in my accent, the 'air r' in Aaron is different to the 'air r' in aerate.
+
+
+
+  
+why is
+clowned::VBD/VBN/JJ: { k l * ow n }> d > :{clown}>ed>:4
+but
+
+
 
   
 I've found missing pronunciations for "lithe"
@@ -19,6 +32,8 @@ HRAO*ET https://youtu.be/-5FEgL-BkIk?si=1XSGHabJnnN6g-fg&t=10239
 
 I've also found voiced and unvoiced versions for "lithe", but I don't know of any steno theory that makes a distinction so that doesn't interest me
 
+
+The word "outworkings" is missing,
 
 
 
@@ -35,6 +50,7 @@ from convert_unilex_into_readable_lists import (
 
 from map_steno_chords_to_keysymbols import generate_write_outs
 
+from chord_definitions import steno_chords_and_their_meanings
 
 
 
@@ -42,25 +58,22 @@ from map_steno_chords_to_keysymbols import generate_write_outs
 
 
 
-def make_input_into_dictionary_entry(input):
+
+def make_input_into_dictionary_entry(input, user_chords):
     word = full_entry_pattern.fullmatch(input).groupdict()
 
     word['pronunciation'           ] = make_target_pronunciation_into_string(make_boundaries_into_list(word['pronunciation']))
     word['word_boundaries'         ] = make_target_spelling_into_string(make_boundaries_into_list(word['word_boundaries']))
-    #print(word)
     word['number of entries'   ] = (0)
-    word['steno stuff'         ] = generate_write_outs(word)
+    word['steno stuff'         ] = generate_write_outs(word, user_chords)
     word['number of entries'   ] = len(word['steno stuff'])
-
-    #print(word)
 
 
 
     word['pronunciation'           ] = str(word['pronunciation'])
     word['word_boundaries'           ] = str(word['word_boundaries'])
     return word
-    return {word['word']: (word)}
-    return {word['word']:word}
+
 
 
 with (open("big.txt", "r", encoding="utf-8")) as txt_dictionary:
@@ -69,37 +82,15 @@ with (open("big.txt", "r", encoding="utf-8")) as txt_dictionary:
 
 
 #for outline in outlines:
-#    results = make_input_into_dictionary_entry(outline)
+#    results = make_input_into_dictionary_entry(outline, steno_chords_and_their_meanings)
 
 
 
+#using multiple inputs
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+    results = pool.starmap(make_input_into_dictionary_entry, [(outline, steno_chords_and_their_meanings) for outline in outlines])
 
-    results = pool.map(make_input_into_dictionary_entry, outlines)
 
-with open("Egg_output.json", "w") as outfile:
+with open("Froj_output.json", "w") as outfile:
     json.dump(results, outfile, indent=1)
 
-#print(results)
-
-
-"""
-Example of an entry:
-
-dictionary['["seriously"],["RB"]'] = [
-
-            #phonetics
-            [[[["s"] ["*"] ["ir"] ["."] ["r"] ["ii"] ["@"] ["s"]],["root"]],[["l iy"],["suffix"]]],
-
-            #orthography
-            [[["serious"],["root"]],          [["ly"],["suffix"]]],
-
-            #write outs
-            ["SAOER/KWRAOE/KWRUS/HREU" 1],
-
-            #briefing within word boundaries
-            ["SAOER/KWHUS/HREU" 1, "SAO*ERD/KWRUS/HREU" 2,"SAOERS/HREU" 3],
-
-            #briefing between word boundaries
-            ["SAOER/KWRAOE/KWRULS" 2, "SAOER/KWHULS" 3, "SAO*ERD/KWRULS" 3,"SAOER/KWRULS" 4,"SAO*ERLSZ" 5, "SAOERLS" 5,"SHRAOERLS" 6]]
-"""
