404b664885821463fb4b1e170c0afa38b95dd636
StenoHarri <159228685+StenoHarri@users.noreply.github.com>
2025-02-27 09:40:31 +0000

    set dictionaries to go into one dictionary "lookup"

--- FrojBot/responses.py
+++ FrojBot/responses.py
@@ -2,69 +2,47 @@
 import json
 import re
 
+lookup = {}
+
 with open("Froj_theories/Froj_Harri_theory/best_of_word_to_entry_lookup.json", "r") as f:
-    best_words = json.load(f)
-with open("Froj_theories/Froj_Harri_theory/all_word_to_entry_lookup.json", "r") as f:
-    all_words = json.load(f)
-with open("Froj_theories/Froj_Harri_theory/Froj_Plover_dictionary.json", "r") as f:
-    entries = json.load(f)
+    lookup['word_lookup'] = json.load(f)
+
 with open("Froj_theories/Froj_Harri_theory/Froj_verbose_lookup.json", "r") as f:
-    verbose_entries = json.load(f)
+    lookup['entry_lookup'] = json.load(f)
+
 
 is_raw_steno = re.compile(r'^(S?T?K?P?W?H?R?[AO*\-EU]+F?R?P?B?L?G?T?S?D?Z?)(/S?T?K?P?W?H?R?[AO*\-EU]+F?R?P?B?L?G?T?S?D?Z?)*$')
 
 
+
+def get_annotation_level(word_to_find):
+
+    if word_to_find.startswith(":>>"):
+        return word_to_find.replace(":>>","").strip(), True
+
+    else: # it must be :>
+        return word_to_find.replace(":>","").strip(), False
+
+
 def get_response( user_input:str) -> str:
-    word_to_find = user_input
 
-
-    if word_to_find.startswith(":>>> "):
-        word_to_find=word_to_find.replace(":>>> ","").strip()
-        complexity = 'very complex'
-    elif word_to_find.startswith(":>>>"):
-        word_to_find=word_to_find.replace(":>>> ","").strip()
-        complexity = 'very complex'
-
-
-    elif word_to_find.startswith(":>> "):
-        word_to_find=word_to_find.replace(":>> ","").strip()
-        complexity = 'complex'
-    elif word_to_find.startswith(":>>"):
-        word_to_find=word_to_find.replace(":>>","").strip()
-        complexity = 'complex'
-
-
-    elif word_to_find.startswith(":> "):
-        word_to_find=word_to_find.replace(":> ","").strip()
-        complexity = 'simple'
-    elif word_to_find.startswith(":>"):
-        word_to_find=word_to_find.replace(":>","").strip()
-        complexity = 'simple'
-    
-    else:
-        return
-
+    word_to_find, complexity = get_annotation_level(user_input)
 
     if word_to_find == '':
-        return 'you can do :> for lookup, :>> for complex lookup'
+        return 'you can do :> for lookup, :>> for annotated lookup'
 
 
     if re.search(is_raw_steno, word_to_find):
-        if word_to_find in entries:
-            if complexity == 'simple':
-                return f"That is in my shorthand dictionary!\n`{word_to_find}` → `{entries[word_to_find]}`"
-            else:
-                output = f"That is in my shorthand dictionary!\n`{word_to_find}` → `{entries[word_to_find]}`"
-                output+= "\n```"
-                for chord in verbose_entries[word_to_find]['explanation']:
-                    output += f"\n\t{chord}"
-                output+="\n```"
-                return output
-        else:
-            return f"Nothing I have maps to {word_to_find}"
+
+        type_of_lookup = 'entry lookup'
+    else:
+        type_of_lookup = 'word lookup'
+        word_to_find = word_to_find.lower()
 
 
-    word_to_find = word_to_find.lower()
+    
+
+
 
     if 'hello' in word_to_find:
         return f"Hi friend (ΘoΘ )"
@@ -74,13 +52,15 @@ def get_response( user_input:str) -> str:
         return "🦐"
     elif 'crazy' in word_to_find:
         return "I was crazy once"
-    elif f"{word_to_find}:" in best_words:
-
-        #if complexity == 'very complex':
-
+    elif type_of_lookup == 'entry lookup' and word_to_find in lookup['entry_lookup']:
+        return f"hi"
+    elif type_of_lookup == 'word lookup' and f"{word_to_find}:" in lookup['word_lookup']:
+        return f"hello"
 
         response = ""
-        response+= f'found `{word_to_find}`'
+        response+= f'found `{word_to_find}` in Tad theory'
+
+
 
         if best_words[word_to_find+':']['text'] == "showing the best 0 out of 0 entries":
             return "Sorry, Harri hasn't written all the rules to make that word. Removing suffixes *might* help"
@@ -120,11 +100,9 @@ def get_response( user_input:str) -> str:
             response+= f"```"
 
         return response
-        return f"Found {word_to_find}\n{best_words[word_to_find+':']['text']}\n{best_words[word_to_find+':']['entries']}"
-        #,{words['{word_to_find}:']['text']}"
-        return best_words["{word_to_find}:"]
+
     else:
-        return "Sorry, I'm missing the pronunciation data for that word :("
+        return "```ANSI\n\033[31mHello\033[0mSorry, I'm missing the pronunciation data for that word :(```"
 
 
 
