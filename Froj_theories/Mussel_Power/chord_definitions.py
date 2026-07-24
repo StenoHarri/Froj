d07fb69af20ee7d178ecf45010e0b5284403dbd6
StenoHarri <stenofrowaway@gmail.com>
2026-07-24 14:47:23 +0100

    long o

--- Froj_theories/Mussel_Power/chord_definitions.py
+++ Froj_theories/Mussel_Power/chord_definitions.py
@@ -32,6 +32,8 @@ so I'll define them once here
 # NothingRegex = re.compile(r'')
 # AtLeastOneCharacterRegex = re.compile(r'.+')
 
+initial_slash = re.compile(r'^/$')
+ends_in__ = re.compile(r'_$')
 
 
 
@@ -40,7 +42,64 @@ Chord: [[spelling,          sound,          briefiness, theory]]
 """
 steno_chords_and_their_meanings = {
 
+    "_": [
+        {"chord": "",
+         "description": "stress",
+         "spelling": "",
+         "pronunciation": " stressed ",
+         "ambiguity": 1,
+         "orthoscore": 0,
+         "what must come before": initial_slash,
+         "theory": ""},
+    ],
 
+
+    "OE": [
+        {"chord": "OE",
+         "description": "OE vowel",
+         "spelling": "o[eu]?",
+         "pronunciation": vowel_category["OE"],
+         "ambiguity": 0,
+         "orthoscore": 0,
+         "what must come before": ends_in__,
+         "theory": ""},
+
+        {"chord": "OE",
+         "description": "OE vowel spelt ow",
+         "spelling": "owe?",
+         "pronunciation": vowel_category["OE"],
+         "ambiguity": 0,
+         "orthoscore": -1,
+         "what must come before": ends_in__,
+         "theory": ""},
+
+        {"chord": "OE",
+         "description": "OE vowel spelt au",
+         "spelling": "au",  # baudelaire, aubergine beaux,
+         "pronunciation": vowel_category["OE"],
+         "ambiguity": 1,
+         "orthoscore": -1, #aubergine
+         "what must come before": ends_in__,
+         "theory": ""},
+
+        {"chord": "OE",
+         "description": "OE vowel",
+         "spelling": "ot$",
+         "pronunciation": vowel_category["OE"],
+         "ambiguity": 0,
+         "orthoscore": 0,
+         "what must come before": ends_in__,
+         "theory": ""},
+
+        {"chord": "OE",
+         "description": "OE vowel spelt oa",
+         "spelling": "oa",
+         "pronunciation": vowel_category["OE"],
+         "ambiguity": 0,  # 0 ambiguity because toad > towed... load >_< lode
+         "orthoscore": -1, #toad
+         "what must come before": ends_in__,
+         "theory": ""},
+    ],
 
 }
 
