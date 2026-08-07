a04eeb581a67bcfab0b86b55bbd999eadd604a37
StenoHarri <stenofrowaway@gmail.com>
2026-08-07 18:59:12 +0100

    All drops happen before a stroke boundary

--- Froj_theories/Mussel_Power/chord_definitions.py
+++ Froj_theories/Mussel_Power/chord_definitions.py
@@ -71,34 +71,6 @@ steno_chords_and_their_meanings = {
          "orthoscore": 0,
          "what must come before": initial,
          "theory": ""},
-
-        {"chord": "/",
-         "description": "drop silent vowel",
-         "spelling": "[aiuo]",  # merciful, somethingcal, adoration (silent o)
-         "pronunciation": "",
-         "ambiguity": 1,
-         "orthoscore": 0,
-         "what must come before": initial,
-         "theory": ""},
-
-        {"chord": "/",
-         "description": "drop short vowel",
-         "spelling": "[aeiouy]+",
-         # this may be a mistake adding the +, but my reasoning is ferrous, anxious, that `ou` is a short @
-         "pronunciation": vowel_category["short"],
-         "ambiguity": 2,
-         "orthoscore": 0,
-         "what must come before": initial,
-         "theory": ""},
-
-        {"chord": "/",
-         "description": "drop long vowel",
-         "spelling": "[aeiouy]",
-         "pronunciation": f'({vowel_category["AOE"]}|{vowel_category["AOEU"]}|{vowel_category["AOU"]}|{vowel_category["AOU"]}|{vowel_category["AEU"]}|{vowel_category["AU"]}|{vowel_category["OE"]}|{vowel_category["OEU"]}|{vowel_category["OU"]}|{vowel_category["EU"]})',
-         "ambiguity": 3,
-         "orthoscore": 0,
-         "what must come before": initial,
-         "theory": ""},
     ],
 
 
@@ -157,34 +129,6 @@ steno_chords_and_their_meanings = {
          "spelling": "",
          "pronunciation": "",
          "ambiguity": 1,
-         "orthoscore": 0,
-         "what must come before": final,
-         "theory": ""},
-
-        {"chord": "/",
-         "description": "drop silent vowel",
-         "spelling": "[aiuo]",  # merciful, somethingcal, adoration (silent o)
-         "pronunciation": "",
-         "ambiguity": 1,
-         "orthoscore": 0,
-         "what must come before": final,
-         "theory": ""},
-
-        {"chord": "/",
-         "description": "drop short vowel",
-         "spelling": "[aeiouy]+",
-         # this may be a mistake adding the +, but my reasoning is ferrous, anxious, that `ou` is a short @
-         "pronunciation": vowel_category["short"],
-         "ambiguity": 2,
-         "orthoscore": 0,
-         "what must come before": final,
-         "theory": ""},
-
-        {"chord": "/",
-         "description": "drop long vowel",
-         "spelling": "[aeiouy]",
-         "pronunciation": f'({vowel_category["AOE"]}|{vowel_category["AOEU"]}|{vowel_category["AOU"]}|{vowel_category["AOU"]}|{vowel_category["AEU"]}|{vowel_category["AU"]}|{vowel_category["OE"]}|{vowel_category["OEU"]}|{vowel_category["OU"]}|{vowel_category["EU"]})',
-         "ambiguity": 3,
          "orthoscore": 0,
          "what must come before": final,
          "theory": ""},
